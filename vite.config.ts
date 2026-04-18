f5c040793eb4758758a7fa08818660be67ea1aa5
KarimDouieb <karim@jetpack.ai>
2026-04-18 17:59:31 +0200

    fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/PareidoMap/',
+  base: '/pareidomap/',
   plugins: [react()],
   resolve: {
     alias: {
