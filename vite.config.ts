e7ad4ab6b956fe8779c1a784d03550ec564a0bc1
KarimDouieb <karim@jetpack.ai>
2026-04-18 17:53:43 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: '/PareidoMap/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': new URL('./src', import.meta.url).pathname,
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
